5b09ef615af978719775442bc0f1fdb53c8a953f
李澤洋 <taro.melillo@gmail.com>
2024-03-04 08:01:55 -0300

    Info.tsx modified

--- src/components/ui/info/Info.tsx
+++ src/components/ui/info/Info.tsx
@@ -3,8 +3,32 @@ export const Info = () => {
     <section className='flex flex-col gap-10 my-10'>
       <div>
         <h3 className='text-xl uppercase font-bold tracking-widest'>
+          Booking Information:{' '}
+        </h3>
+        <br />
+        <p>
+          bale 4000 PHP po ang rent ng buong place (pool, bar, gazebo, lounge or
+          rest area), exclusive for your group or family nyu lang po, good for
+          15 pax po sya, and additional 200 PHP per head po for extra persons
+          po. Our limit is only up to 25 people po. From 9 a.m to 6 p.m po
+          coverage ng rent. No corkage fee po tyo kaya libre po magdala ng khit
+          anong foods po. Family oriented po yung place kay strictly no hard
+          liquors po but allowed drinks like light beers and wine.
+        </p>
+        <br />
+        <p>
+          For confirmation of booking, a non refundable (but can be re-booked as
+          long as the date specified is available and within 3 days before your
+          booked date) reservation fee of 500 PHP shall be collected. The amount
+          will be deducted to the total booking fee upon check in. Can be paid
+          thru g-cash or directly on site.
+        </p>
+      </div>
+      <div>
+        <h3 className='text-xl uppercase font-bold tracking-widest'>
           Address:
         </h3>
+        <br />
         <p>
           Purok 7, Bakras, Bulanao, Tabuk City, Kalinga, Tabuk, Philippines,
           3800
@@ -14,6 +38,7 @@ export const Info = () => {
         <h3 className='text-xl uppercase font-bold tracking-widest'>
           Schedule:{' '}
         </h3>
+        <br />
         <p>We are open from 9 a.m. to 6 p.m.</p>
       </div>
     </section>
