eafc714d5dd75cceaad858d19b7e41a9fd25b5d8
Lautaro Nicolas Melillo <taro.melillo@gmail.com>
2025-07-29 00:00:25 -0400

    Update WhatsAppTemplate.ts

--- src/utlis/WhatsAppTemplate.ts
+++ src/utlis/WhatsAppTemplate.ts
@@ -1,5 +1,5 @@
 export default function WhatsappTemplate(booking: any) {
-  const phoneNumber = 639959764478;
+  const phoneNumber = ;
   const template = `${booking.day} %0A Name: ${booking.name}%0A Lastname: ${booking.lastname}%0A`;
   const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Hello, I've booked the following day:%0A${template} Thanks.`;
   return { url };
